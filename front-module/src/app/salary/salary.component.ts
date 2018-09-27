e2a0069c56c118fc71e84a0aacc075150a27bc42
messouri <mohammed.essouri@gmail.com>
2018-09-28 01:11:18 +0200

    commit des sources test salarie
    commit des sources test salarie

---
+++ front-module/src/app/salary/salary.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'app-salary',
+  templateUrl: './salary.component.html',
+  styleUrls: ['./salary.component.css']
+})
+export class SalaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
